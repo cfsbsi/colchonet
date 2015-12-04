a895e666243f517cba309f4e21522bbd7b51935a
Christian Franco <cfs.bsi@outlook.com>
2015-12-03 22:30:43 -0200

    room_spec and an other create user test added

--- spec/features/creates_a_new_user_spec.rb
+++ spec/features/creates_a_new_user_spec.rb
@@ -22,4 +22,24 @@ feature 'Create a new User' do
     expect(page).to have_content location
     expect(page).to have_content bio
   end
+
+  scenario 'bio length should be bigger than 29' do
+    visit new_user_path
+
+    name = 'Christian Franco'
+    email = 'cfs.bsi@gmail.com'
+    location = 'Sorocaba-SP'
+    password = '123456'
+    bio = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+
+    fill_in 'Nome completo', with: name
+    fill_in 'Localização', with: location
+    fill_in 'E-mail', with: email
+    fill_in 'Senha', with: password
+    fill_in 'Confirme sua senha', with: password
+    fill_in 'Biografia', with: bio
+    click_button 'Create Usuário'
+
+    expect(page).to have_content 'Há erro no formulário, por favor verifique.'
+  end
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,7 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  @room = Room.new
+  it 'persist new room' do
+    title = 'First room'
+    location = 'Sorocaba-SP'
+    description = 'Quarto com uma cama, comoda e ventilador de teto.'
+    room = Room.create(title: title, location: location, description: description)
 
-  pending "add some examples to (or delete) #{__FILE__}"
+    expect(room).to be_persisted
+    expect(room.title).to eq title
+    expect(room.location).to eq location
+    expect(room.description).to eq description
+  end
 end
